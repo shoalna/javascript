9ebee196996f45ab62a1cff6515a36441718d464
shoalna <ljxjplife@gmail.com>
2020-07-25 20:22:01 +0900

    Update exchange_key_value_js.js

--- exchange_key_value_js.js
+++ exchange_key_value_js.js
@@ -3,10 +3,10 @@
 
 // either comporation of blow 2 variables should work
 let a = {"a":"id", "b":"name", "c":"age", "d":"address"};
-let a = {a:"code", b:"name", c:"date", d:"font"};
+let a = {a:"id", b:"name", c:"age", d:"address"};
 
-let b = [{a:2,b:3,c:4},{a:2,b:3,c:4},{a:2,b:3,c:4}];
-let b = [{"a":2,"b":3,"c":4},{"a":2,"b":3,"c":4},{"a":2,"b":3,"c":4}];
+let b = [{a:2,b:3,c:4},{a:2,b:3,c:4},{a:2,b:3,d:5}]; // notice 3rd element is not same with other
+let b = [{"a":2,"b":3,"c":4},{"a":2,"b":3,"c":4},{"a":2,"b":3,"d":5}];
 
 let c = b.map((item, index, arr) => {
     console.log(item);  // always be [{a:2,b:3,c:4},{a:2,b:3,c:4},{a:2,b:3,c:4}]
@@ -17,3 +17,5 @@ let c = b.map((item, index, arr) => {
     }
     return obj;
 });
+
+console.log(c); // [{id:2,name:3,age:4},{id:2,name:3,age:4},{id:2,name:3,address:5}]
