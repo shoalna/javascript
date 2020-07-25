e961d6505ee9e22e06ba216a47845ebd025b2a02
shoalna <ljxjplife@gmail.com>
2020-07-25 19:02:27 +0900

    Update exchange_key_value_js.js

--- exchange_key_value_js.js
+++ exchange_key_value_js.js
@@ -4,11 +4,10 @@ let b = [{a:2,b:3,c:4},{a:2,b:3,c:4},{a:2,b:3,c:4}];
 
 
 let c = b.map((item, index, arr) => {
-    console.log("==========");
-    console.log(item);
+    console.log(item);  // always be [{a:2,b:3,c:4},{a:2,b:3,c:4},{a:2,b:3,c:4}]
     let obj = {};
     for(let i in item){
-       console.log(i);
+       console.log(i); // a, b, or c
        obj[a[i]] = item[i];
     }
     return obj;
